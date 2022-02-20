0df705f4a240e343cc2e1df02dd635ee686bc1ca
Conor Kennedy <conor@conorkennedy.com>
2022-02-19 21:27:34 -0500

    filter on search and refactoring

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card  id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email}/>
-    <Card  id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
-    <Card  id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
